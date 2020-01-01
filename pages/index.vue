9f7b19ebd88484029f13671a412f403a580c9e1f
Sergey Stoma <sergey@underclouds.com>
2020-01-01 13:40:19 -0500

    Add support links

--- pages/index.vue
+++ pages/index.vue
@@ -68,7 +68,8 @@
             ><path d="M288 29V16a16 16 0 0 0-16-16H160a16 16 0 0 0-16 16v16a16 16 0 0 0 16 16h58.12l-82.2 93.94A32 32 0 0 0 128 163v13a16 16 0 0 0 16 16h112a16 16 0 0 0 16-16v-16a16 16 0 0 0-16-16h-58.13l82.21-93.94A32 32 0 0 0 288 29zm-88 227H32a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h99.34L9.53 440.06A32.09 32.09 0 0 0 0 462.86V488a24 24 0 0 0 24 24h184a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16H92.66l121.81-120.06a32.09 32.09 0 0 0 9.53-22.8V280a24 24 0 0 0-24-24zm232-32H320a16 16 0 0 0-16 16v16a16 16 0 0 0 16 16h58.12l-82.2 93.94A32 32 0 0 0 288 387v13a16 16 0 0 0 16 16h112a16 16 0 0 0 16-16v-16a16 16 0 0 0-16-16h-58.13l82.21-93.94A32 32 0 0 0 448 253v-13a16 16 0 0 0-16-16z" /></svg>.
             <a href="https://www.termsfeed.com/privacy-policy/f0bfd7e40e92346fa5b05c5f0fe51a2b">Privacy Policy</a>
             and
-            <a href="https://www.termsfeed.com/terms-conditions/5b2690df517070518303bd8e32c80b91">Terms of Use</a>
+            <a href="https://www.termsfeed.com/terms-conditions/5b2690df517070518303bd8e32c80b91">Terms of Use</a>.
+            <a href="https://github.com/sergeystoma/updoot/issues">Issues &amp; Support.</a>
         </footer>
     </div>
 </template>
