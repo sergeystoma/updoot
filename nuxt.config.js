a414c4ddaf451f03cdbd31528373883e0f56fe3c
Sergey Stoma <sergey@underclouds.com>
2021-07-29 20:57:03 -0400

    Keep login session cookie for longer

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,9 @@ const config = {
             secret: process.env.SESSION_SECRET,
             resave: false,
             saveUninitialized: false,
+            cookie: {
+                maxAge: 24 * 60 * 60 * 1000 * 30 * 12,
+            },
         }),
         {
             path: 'api/login',
